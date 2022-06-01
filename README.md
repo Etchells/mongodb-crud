d0ad1c9e6ad9f179007cdb963422b59d8fab881b
Glenn Etchells <glenn.etchells@outlook.com>
2022-06-01 14:21:33 +0100

    model, repo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mongodb-crud
+
+Steps
+
+1 Download Docker Desktop
+
+2 Run docker-compose.yaml file (You will need docker plug-in)
+
+3 Goto http://localhost:8081/ and create database with the same name stated in application.properties
+
+
+
+
+
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.model.Address;
+import com.example.demo.model.Gender;
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +23,42 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(StudentRepository studentRepository, MongoTemplate mongoTemplate){
+		return args -> {
+			Address address = new Address(
+					"England",
+					"London",
+					"NE9"
+			);
+
+			String email = "bob@bobby.com";
+
+			Student student = new Student(
+					"Bob",
+					"Bobby",
+					email,
+					Gender.MALE,
+					address,
+					List.of("Computer Science"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+
+			List<Student> students = mongoTemplate.find(query,Student.class);
+
+			if (students.size() > 1){
+				throw new IllegalStateException("found many students with email " + email);
+			}
+
+			if (students.isEmpty()){
+				System.out.println("Inserting student " + student);
+				studentRepository.insert(student);
+			}else {
+				System.out.println(student + " already exists");
+			}
+		};
+	}
 }
